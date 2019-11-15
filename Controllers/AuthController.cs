8ed1471b4d06f28a2ee43d1fe937e043f6498d9b
fabiokaspar <fabiokaspar@gmail.com>
2019-11-15 20:41:21 -0300

    Added updating user action in Auth controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using BigOferta.API.Data;
 
 namespace BigOferta.API.Controllers
 {
@@ -24,47 +25,51 @@ namespace BigOferta.API.Controllers
         private readonly IMapper _mapper;
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly DatingRepository _repo;
 
         public AuthController(IConfiguration config, IMapper mapper,
-        UserManager<User> userManager, SignInManager<User> signInManager)
+            UserManager<User> userManager, SignInManager<User> signInManager,
+            DatingRepository repo)
         {
+            this._repo = repo;
             this._signInManager = signInManager;
             this._userManager = userManager;
             this._mapper = mapper;
             this._config = config;
         }
-        
+
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
             User userToCreate = _mapper.Map<User>(userForRegisterDto);
             var result = await _userManager.CreateAsync(userToCreate, userForRegisterDto.Password);
-           
-           if (result.Succeeded)
-           {
-                UserForDetailDto userToReturn = _mapper.Map<UserForDetailDto>(userToCreate);
-                
-                return Ok(userToReturn);
-           }
 
-           return BadRequest(result.Errors);    
+            if (result.Succeeded)
+            {
+                UserForReturnDto userToReturn = _mapper.Map<UserForReturnDto>(userToCreate);
+
+                return Ok(userToReturn);
+            }
+
+            return BadRequest(result.Errors);
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            User userFromStore = await _userManager.FindByNameAsync(userForLoginDto.Username);
-            
+            User userFromStore = await _userManager.FindByNameAsync(userForLoginDto.UserName);
+
             if (userFromStore == null)
                 return Unauthorized();
-            
+
             var result = await _signInManager.CheckPasswordSignInAsync(userFromStore, userForLoginDto.Password, false);
-            
+
             if (result.Succeeded)
             {
-                var userToReturn = _mapper.Map<UserForDetailDto>(userFromStore);
+                var userToReturn = _mapper.Map<UserForReturnDto>(userFromStore);
 
-                return Ok(new {
+                return Ok(new
+                {
                     token = GenerateJwtToken(userFromStore),
                     user = userToReturn
                 });
@@ -89,7 +94,7 @@ namespace BigOferta.API.Controllers
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddHours(2),
+                Expires = DateTime.Now.AddDays(1),
                 SigningCredentials = creds
             };
 
@@ -99,5 +104,42 @@ namespace BigOferta.API.Controllers
 
             return serializedToken;
         }
+
+        [HttpPut("{userId}/update")]
+        [Authorize]
+        public async Task<IActionResult> UpdateAccountUser(int userId, UserForUpdateDto userForUpdateDto)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            User user = await _repo.GetUser(userId);
+            
+            _mapper.Map<UserForUpdateDto, User>(userForUpdateDto, user);
+            var result = await _userManager.UpdateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            if (userForUpdateDto.CurrentPassword != null &&
+                userForUpdateDto.CurrentPassword.Trim().Length > 0 &&
+                userForUpdateDto.NewPassword != null &&
+                userForUpdateDto.NewPassword.Trim().Length > 0)
+            {
+                result = await _userManager.ChangePasswordAsync(user, 
+                    userForUpdateDto.CurrentPassword, userForUpdateDto.NewPassword);
+                
+                if (!result.Succeeded)
+                {
+                    return BadRequest(result.Errors);
+                }
+            }
+
+            UserForReturnDto userForReturnDto = _mapper.Map<UserForReturnDto>(user);
+
+            return Ok(userForReturnDto);
+        }
+
     }
 }
